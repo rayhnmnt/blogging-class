9895cde27436c0e0b70ea261c710ce30577dc2c8
rayhnmnt <rayhinmanto1@gmail.com>
2023-10-31 20:29:01 +0700

    new blogging P.Fredinan

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,11 @@ Route::get('/contact-us', [ContactController::class, 'index'])->name('contact-us
 Route::post('/contact-us', [ContactController::class, 'store'])->name('contact-us.store');
 
 Route::resource('articles', ArticleController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
